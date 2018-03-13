60e4f19ed62f191c43c614160ea3d4aec2ec32d1
Katie Davis <katieannedavis@users.noreply.github.com>
2018-03-13 08:46:29 -0400

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,14 @@
+# Random number generator - https://www.calculatorsoup.com/calculators/statistics/number-generator.php
+toSort = [66, 82, 85, 69, 85, 43, 38, 84, 42, 55, 56, 70, 4, 70, 90, 85, 46, 32, 66, 30]
+n = 0
+for x in toSort:
+    for index in toSort:
+        try:
+            if toSort[n] > toSort[n+1]:
+                toSort[n], toSort[n+1] = toSort[n+1], toSort[n]
+        except IndexError:
+            n = 0
+            continue
+        n += 1
+print(toSort)
+
